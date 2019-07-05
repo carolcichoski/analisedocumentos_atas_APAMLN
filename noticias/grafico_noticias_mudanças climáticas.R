37e2865f35618c0f50246f2610da761ff63d85bc
Caroline Cichoski <cacacichoski@gmail.com>
2019-07-04 21:26:09 -0300

    Gráfico das saliências
    Acrescentado gráfico do clima

--- noticias/grafico_noticias_mudanças climáticas.R
+++ noticias/grafico_noticias_mudanças climáticas.R
@@ -1,29 +1,28 @@
 library(plotly)
 library(readr)
 library(tidyverse)
-library(readr)
-noticias_clima <- read_delim("noticias/noticias_clima.csv", 
-                             ";", escape_double = FALSE, locale = locale(encoding = "ISO-8859-1"), 
-                             trim_ws = TRUE)
-View(noticias_clima)
+noticias <- read_delim("noticias/noticias.csv", 
+                       ";", escape_double = FALSE, locale = locale(encoding = "ISO-8859-1"), 
+                       trim_ws = TRUE)
+View(noticias)
 
 
-p <- noticias_clima %>% 
+p <- noticias %>% 
   group_by(ameacas) %>% 
   mutate(n_total = sum(quantidade)) %>% 
   ggplot(aes(x = reorder(ameacas, -n_total),  y = quantidade, fill = ano)) +
   geom_bar(stat = "identity", position = "stack")+
   coord_flip() +
   theme_minimal()+
-  labs(y="Número de Noticías", x= "Ameaça", 
-       title = "Noticías citando Mudanças Climáticas - Jan/2015 a Mai/2019")+
+  labs(y="Número de Noticías", x= "Ameaças", 
+       title = "Noticías citando as Ameaças - Jan/2015 a Mai/2019")+
   theme(plot.title = element_text(hjust = 0.5),
         text=element_text(size=12,  family="Times New Roman"))+
   scale_fill_distiller(palette = "Paired")+
-  scale_y_continuous(breaks = seq(0, 31,by = 5))
+  scale_y_continuous(breaks = seq(0, 440, by = 40))
 
 
-by = 5
+
 
 
 plotly::ggplotly(p)
