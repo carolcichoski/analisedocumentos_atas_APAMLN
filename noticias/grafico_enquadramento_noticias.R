087496be247bac99132bbf3bf9ce8885827497fe
Caroline Cichoski <cacacichoski@gmail.com>
2019-07-05 02:34:09 -0300

    Graficos de saliência
    Graficos de saliência com ponto
    ameaças
    beneficios
    importância está ruim

---
+++ noticias/grafico_enquadramento_noticias.R
@@ -0,0 +1,30 @@
+library(plotly)
+library(readr)
+library(tidyverse)
+library(readr)
+
+
+enquadramento <- read_delim("noticias/enquadramento.csv", 
+                            ";", escape_double = FALSE, locale = locale(encoding = "ISO-8859-1"), 
+                            trim_ws = TRUE)
+View(enquadramento)
+
+enquadramento %>% 
+  group_by(enquadramento) %>% 
+  mutate(n_total = sum(quantidade)) %>% 
+  ggplot(aes(x = reorder(enquadramento, -n_total),  y = quantidade, fill = ano)) +
+  geom_bar(stat = "identity", position = "stack")+
+  coord_flip() +
+  theme_minimal()+
+  labs(y="Número de notícias", x= "Categorias de enquadramento", 
+       title = "Enquadramento das notícias sobre Mudanças Climáticas")+
+  theme(plot.title = element_text(hjust = 0.5),
+        text=element_text(size=12,  family="Times New Roman"))+
+  scale_fill_distiller(palette = "Paired")+
+  scale_y_continuous(breaks = seq(0, 12,by = 2))
+
+
+
+
+
+plotly::ggplotly(p)
---
+++ saliencia/grafico_ameacas_ultimatent.R
@@ -0,0 +1,55 @@
+library(readr)
+library(tidyverse)
+library(readr)
+library(readr)
+ameacas_ponto <- read_delim("~/GitHub/analisedocumentos_atas_APAMLN/saliencia/ameacas_ponto.csv", 
+                            ";", escape_double = FALSE, locale = locale(encoding = "ISO-8859-1"), 
+                            trim_ws = TRUE)
+View(ameacas_ponto)
+
+#grafico com Frequencia
+ameacas_ponto %>% 
+  group_by(risco) %>% 
+  mutate(n_total = sum(indice)) %>% 
+  ggplot(aes(x = reorder(risco, n_total),  y = indice, fill = Frequência))+
+  geom_bar(stat = "identity", position = "stack")+
+  coord_flip() +
+  theme_minimal()+
+  labs(y="Índice de Saliência", x= "Ameaças",
+       title = "Gráfico do Índice de Saliência das Ameaças")+
+  theme(plot.title = element_text(hjust = 0.5),
+        text=element_text(size=12,  family="Times New Roman"))+
+  scale_fill_distiller(palette = "Paired")
+
+
+
+#Gráfico correto de colunas
+ameacas_ponto %>% 
+  group_by(risco) %>% 
+  mutate(n_total = sum(indice)) %>% 
+  ggplot(aes(x = reorder(risco, n_total),  y = indice))+
+  geom_col(colour="black", fill= "white") +
+  coord_flip() +
+  theme_minimal()+
+  labs(y="Índice de Saliência", x= "Ameaças", 
+       title = "Gráfico do Índice de Saliência das Ameaças")+
+  theme(plot.title = element_text(hjust = 0.5),
+        text=element_text(size=12,  family="Times New Roman"))+
+ # scale_y_discrete(breaks = seq(0, 1.0,by = 0.2)) 
+  
+  
+  #não consigo mudar a escala
+  
+
+#GRAFICO QUE DEU MAIS CERTO
+ameacas %>%
+  ggplot(aes(risco, indice))+
+  geom_col(colour="black", fill= "white") +
+  coord_flip() +
+  theme_minimal()+
+  labs(X ="Ameaças", Y= "Índice de Saliência", 
+       title = "Gráfico da Saliência das Ameaças Percebidas")+
+  scale_y_discrete(breaks = seq(0, 1.0,by = 0.2))
+
+  
+ 
